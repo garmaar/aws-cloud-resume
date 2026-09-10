991bb03c3cdbb107f1135b54dccf7c0a60d7fd87
garmaar <pablogarmar04@gmail.com>
2026-09-10 16:45:40 +0200

    web design improvement

--- site/src/scripts.js
+++ site/src/scripts.js
@@ -1,10 +1,26 @@
-const API_URL = "https://81zwv999m0.execute-api.us-east-1.amazonaws.com/visits";
+const API_URL =
+"https://81zwv999m0.execute-api.us-east-1.amazonaws.com/visits";
+
 
 fetch(API_URL)
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById("visitor-count").textContent = data.visits;
-  })
-  .catch(error => {
-    console.error("Error fetching visitor count:", error);
-  });
+
+.then(response => response.json())
+
+.then(data => {
+
+    document.getElementById("visits-count").textContent =
+    data.visits;
+
+})
+
+.catch(error => {
+
+    console.error(
+        "Error loading visits:",
+        error
+    );
+
+    document.getElementById("visits-count").textContent =
+    "Unavailable";
+
+});
